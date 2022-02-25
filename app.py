defe35bec5d00363569c12196962fe408c990904
peagull <paololamadrid98@gmail.com>
2022-02-25 11:59:45 +0000

    can click thread titles for more detailed view

--- app.py
+++ app.py
@@ -7,15 +7,22 @@ test_threads = {
     "1": {
         "title": "Hello there!",
         "date": datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+        "desc": "This is a test thread!"
     },
     "2": {
         "title": "Yo!",
         "date": datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+        "desc": "Whassssssssuuuuuuup!"
     },
 }
 
 @app.route("/")
 def index():
-    threads = test_threads.values()
 
-    return render_template("index.html", threads=threads)
+    return render_template("index.html", threads=test_threads)
+
+@app.route("/<thread_id>")
+def thread(thread_id):
+    thread = test_threads[thread_id]
+
+    return render_template("thread.html", thread=thread)
